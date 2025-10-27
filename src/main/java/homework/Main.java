86f0e6d83fe70c07e9f1a2e6f64ac924e2dedcb3
Sergei Bychkov <sergei.by4kov.93@gmail.com>
2025-10-27 11:35:01 +0300

    Выполняем сумму, разность, произведение и частное чисел.

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -4,7 +4,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+
+
+        System.out.println("Введите первое число:");
+        int firstNumber = Integer.parseInt(new Scanner(System.in).nextLine());
+
+        System.out.println("Введите второе число:");
+        int secondNumber = Integer.parseInt(new Scanner(System.in).nextLine());
+
+        int sum = (int) secondNumber + (int) firstNumber;
+        System.out.println("Cумма чисел:" + sum);
+
+        int difference = (int) secondNumber - (int) firstNumber;
+        System.out.println("Разность чисел:" + difference);
+
+        int product = (int) secondNumber * (int) firstNumber;
+        System.out.println("Произведение чисел:" + product);
+
+        double quotient = (double) secondNumber / firstNumber;
+        System.out.println("Частное чисел:" + quotient);
     }
 }
