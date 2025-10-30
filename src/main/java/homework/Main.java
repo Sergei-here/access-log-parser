8d7b38a468ad5d9944e9b44edcc62df260273a4d
Sergei-here <sergei.by4kov.93@mail.ru>
2025-10-30 08:55:10 +0300

    Revert "2 number experiments"

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -4,24 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        System.out.println("Введите первое число:");
-        int firstNumber = Integer.parseInt(new Scanner(System.in).nextLine());
-
-        System.out.println("Введите второе число:");
-        int secondNumber = Integer.parseInt(new Scanner(System.in).nextLine());
-
-        int sum = (int) secondNumber + (int) firstNumber;
-        System.out.println("Cумма чисел:" + sum);
-
-        int difference = (int) secondNumber - (int) firstNumber;
-        System.out.println("Разность чисел:" + difference);
-
-        int product = (int) secondNumber * (int) firstNumber;
-        System.out.println("Произведение чисел:" + product);
-
-        double quotient = (double) secondNumber / firstNumber;
-        System.out.println("Частное чисел:" + quotient);
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Длина текста: " + text.length());
     }
 }
