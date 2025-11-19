86ad23f152e25215b284750e84f7b854f99f977b
Sergei Bychkov <sergei.by4kov.93@gmail.com>
2025-11-19 13:05:19 +0300

    Исправление замечаний курсового проекта. Задание по теме "Циклы"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,21 @@ import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int pathFileCount = 0;
         while (true) {
             System.out.println("Введите путь к файлу:");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
             boolean fileExists = file.exists(); // проверка существования файла
-            boolean isFile = file.isFile(); // проверка того что это файл
             boolean isDirectory = file.isDirectory(); // проверка того что это файл, а не папка
-            if (!fileExists || isDirectory) {
-                if (!fileExists) {
-                    System.out.println("Файл не существует");
-                } else if (isDirectory) {
-                    System.out.println("Это папка, а не файл");
-                }
+            if (!fileExists) {
+                System.out.println("Файл не существует");
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println("Это папка, а не файл");
                 continue;
             }
             pathFileCount++;
