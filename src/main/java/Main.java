69964b3993bb7f97a56e40f561d5332a9bc8dfe4
Sergei Bychkov <sergei.by4kov.93@gmail.com>
2025-12-20 23:58:54 +0300

    Задание #1. Обработка исключений

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
@@ -22,6 +24,45 @@ public class Main {
             pathFileCount++;
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + pathFileCount);
+
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader =
+                        new BufferedReader(fileReader);
+                String line;
+                int lineCount = 0;
+                String longestLine = "";
+                String shortestLine = "";
+                int longestLength = 0;
+                int shortestLength = Integer.MAX_VALUE;
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    lineCount++;
+
+                    if (length > longestLength) {
+                        longestLength = length;
+                        longestLine = line;
+                    }
+
+                    if (length < shortestLength) {
+                        shortestLength = length;
+                        shortestLine = line;
+                    }
+
+                    if (length > 1024) {
+                        throw new IllegalArgumentException("Длина строки превышает 1024 символа");
+                    }
+                }
+                System.out.println("Общее количество строк: " + lineCount);
+                System.out.println("Длина самой длинной строки в файле: " + longestLine);
+                System.out.println("Длина самой короткой строки в файле: " + shortestLine);
+            } catch (IllegalArgumentException e) {
+                System.err.println(e.getMessage());
+                System.err.println("Обработка файла прекращена");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
         }
     }
 }
